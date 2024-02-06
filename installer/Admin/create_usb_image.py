c290adb5c720870a133a88970c8c3fc9cbde8e4b
史蒂夫ZMT工作室 <zzhzcuid@outlook.com>
2024-02-06 21:29:39 +0800

    Complete usb installer
    1.complete usb installer  92%
    2.introducing 7z
    3.fix all bugs!!!

--- installer/Admin/create_usb_image.py
+++ installer/Admin/create_usb_image.py
@@ -1,60 +1,209 @@
 import webbrowser #internal module
 import os
 
+
+from colorama import init,Fore
+init(autoreset=True)
+
 def main():
     # this script was used to install ventoy
-    print("        HephaestusOS维护系统 安装程序")
-    os.system("title 'Hephaestus Installer For USB disk'")
+    print("        HephaestusOS维护系统 安装程序 FOSS")
+    os.system("title HephaestusOS维护系统 安装程序-USB版")
     os.system("cls")
     print("        HephaestusOS维护系统 安装程序\n\n")
-    print("首先，我们要从互联网上下载一个名叫Ventoy的便捷化USB引导设备制作工具。\n该工具的项目首页为https://www.ventoy.net/ \n您想现在下载它吗？\n[1]是，现在从互联网上下载 [2]不，我想使用程序内置的版本")
+    #debug option
+    print(__file__)
+    print(detectLocation())
+    #debug end
+    print("首先，我们要从互联网上下载一个名叫Ventoy的便捷化USB引导设备制作工具。\n该工具的项目首页为https://www.ventoy.net/ \n您想现在下载它吗？\n[1]是，现在从互联网上下载 [2]不，我想使用程序内置的版本 [0]关闭程序" )
     detect=input()
-    if detect == "1":
-        if ViewInBrowser("https://sourceforge.net/projects/ventoy/files/v1.0.97") == 1:
-            print("你想再试一次吗？")
+    if detect == "1":#对~就应该从网上弄
+        if ViewInBrowser("https://sourceforge.net/projects/ventoy/files/") == 1:
+            
+            adprint("你想再试一次吗？",3)
             confirm()
-            if ViewInBrowser("https://sourceforge.net/projects/ventoy/files/v1.0.97") == 1:
+            if ViewInBrowser("https://sourceforge.net/projects/ventoy/files/") == 1:
                 print("仍然失败...")
-                print("Error locate:SYSTEM_WEBBROWSER_OPEN_FAIL")
+
+                print("Errorloc:SYSTEM_WEBBROWSER_OPEN_FAIL")
                 print("请检查您的系统环境是否异常或将其反馈给开发者，记得包括系统的详细信息。")
                 return 1
         print("请在打开的浏览器里面选取最新版本的Ventoy_1.0.97_windows.zip,然后将其下载下来。当下载完成后，请把zip文件直接拖入此窗口。")
         detect=""
         detect=input()
-    elif detect == "2":
+        detect=detect.replace("\\","\\\\")
         print("正在解压...")
-        scr_path=detectLocation()[0]
+        if detectExist(detect) == 1:#查头
+            #detect source failed
+            print("无效路径！未能找到拖入的文件\n正在重试...")
+            print("请在打开的浏览器里面选取最新版本的Ventoy_1.0.97_windows.zip,然后将其下载下来。当下载完成后，请把zip文件直接拖入此窗口。")
+            detect=""
+            detect=input()
+            if detectExist(detect) == 1:
+                print("仍然失败... :(")
+                print("ErrorLoc:LOADIN_VENTOY_NOT_FOUND_1")
+                print("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。")
+                return 1
+            
+        
+        
+        ''' # obseleted
+        if rcmd(detectLocation()+"\\source\\7z.exe e "+detect) !=0:
+            #unzip error
+            adprint("无法解压！",1)
+            print("ErrorLoc:LOADIN_VENTOY_UNZIP_FAIL")
+            adprint("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。",3)
+            return 1
+        '''
+        
+        if rcmd("mkdir "+detectLocation()+"\\program\\ventoy\\") !=0:#创标
+            #mkdir system command fail
+            adprint("创建文件夹失败！",11)
+            adprint("复制失败！",11)
+            adprint("参考上方报错，检查系统状态，或者重装此电脑，或者硬盘寿命已尽。",9)
+            adprint("程序将立刻终止。")
+        if rcmd("cp /v"+detect+" "+detectLocation()+"\\") !=0:#拷贝
+            #copy failed
+            adprint("复制失败！",11)
+            adprint("参考上方报错，检查系统状态，或者重装此电脑，或者硬盘寿命已尽。",9)
+            adprint("程序将立刻终止。")
+            return 1
+        if detectExist(detectLocation()+"\\ventoy-1.0.97-windows.zip") == 1:#查标
+            #locate err
+            adprint("定位文件失败！未能找到"+detectLocation()+"\\ventoy-1.0.97-windows.zip\n正在重试...",1)
+            if detectExist(".\\ventoy-1.0.97-windows.zip") == 1:
+                adprint("仍然失败... :(",3)
+                print("ErrorLoc:LOCAL_VENTOY_NOT_FOUND_1")
+                adprint("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。",3)
+                return 1
+        if rcmd(detectLocation()+"\\source\\7z.exe x "+detectLocation()+"\\ventoy-1.0.97-windows.zip -o"+detectLocation()+"\\program\\ventoy\\") !=0:#解压
+            #unzip error
+            adprint("无法解压！",1)
+            print("ErrorLoc:LOCAL_VENTOY_UNZIP_FAIL")
+            adprint("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。",3)
+            return 1
+    elif detect == "2":#你干嘛要用内置一堆bug还有可能过时的内置？？？！！！
+        rcmd("cls")
+        print("正在解压...")
+        # scr_path=detectLocation()
+        # rcmd("cd "+detectLocation())
+        if detectExist(detectLocation()+"\\source\\7z.exe") == 1:#查工具
+            # unzip tool lost
+            adprint("安装不完全！未能找到.\\source\\7z.exe\n正在重试...")
+            if detectExist(".\\source\\7z.exe") == 1:
+                adprint("仍然失败... :(")
+                print("ErrorLoc:7Z_NOT_FOUND_1")
+                adprint("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。",3)
+                return 1
+        if detectExist(detectLocation()+"\\ventoy-1.0.97-windows.zip") == 1:#查标
+            # internal ventoy lost
+            adprint("安装不完全！未能找到.\\ventoy-1.0.97-windows.zip\n正在重试...",1)
+            if detectExist(".\\ventoy-1.0.97-windows.zip") == 1:
+                adprint("仍然失败... :(",3)
+                print("ErrorLoc:LOCAL_VENTOY_NOT_FOUND_1")
+                adprint("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。",3)
+                return 1
+        if rcmd("mkdir "+detectLocation()+"\\program\\ventoy\\") !=0:#创标
+            #mkdir system command fail
+            adprint("创建文件夹失败！",11)
+            adprint("复制失败！",11)
+            adprint("参考上方报错，检查系统状态，或者重装此电脑，或者硬盘寿命已尽。",9)
+            adprint("程序将立刻终止。")
+        if rcmd(detectLocation()+"\\source\\7z.exe x "+detectLocation()+"\\ventoy-1.0.97-windows.zip -o"+detectLocation()+"\\program\\ventoy\\") !=0:
+            #unzip fail
+            adprint("无法解压！",1)
+            print("ErrorLoc:LOCAL_VENTOY_UNZIP_FAIL")
+            adprint("请检查您的系统环境是否异常并尝试重新通过安装向导或附带的光盘进行安装，或将其反馈给开发者，记得包括系统的详细信息。",3)
+            return 1
+    elif detect =="0":
+        return 0
+    else:
+        adprint("不合法的输入。执行默认操作：退出")
+        return 0
+    print("现在将打开ventoy安装程序，请按照界面指示插入一张空白U盘，然后按提示点击“安装”。")
+    pass #breakpoint
+    items = os.listdir(detectLocation()+"\\program\\ventoy\\")
+    folders = [item for item in items if os.path.isdir(item)]
+    for folder in folders:
+        tfolder=folder
+    ventoy_location=detectLocation()+"\\program\\ventoy\\"+tfolder+"\\Ventoy2Disk.exe"
+    if rcmd(ventoy_location) !=0:
+        adprint("打开失败！",11)
+        rcmd("del /q "+detectLocation()+"\\program\\ventoy")
+    
 
 
 
 
-def ViewInBrowser(url):
+def ViewInBrowser(url:str):
     # this func used to open browser with a link (to download sth.)
     print("您即将前往访问",url,"正在打开浏览器...")
     try:
         webbrowser.open_new(url)
     except:
-        print("出现错误，打开浏览器失败！")
+        adprint("出现错误，打开浏览器失败！",1)
         return 1
     return None
 
 def confirm():
     os.system("pause")
+    # anykey=input("[Anykey]继续")
 
 def detectLocation():
     script_path = __file__
     script_directory = os.path.dirname(script_path)
-    return [script_directory,script_path]
+    return script_directory
 
-def detectExist(path):
+def detectExist(path:str):
     if os.path.exists(path):
         return 0
     else:
         return 1
 
+def rcmd(cmd:str):
+    ErrorCode=os.system(cmd)
+    if ErrorCode != 0:
+        adprint("执行语句"+cmd+"时发生了错误，",1)
+        return ErrorCode
+    return 0
 
+def adprint(text:str,color=0,enterln=True):
+    '''
+    Available color
+    [None] = 1 -Red
+    1-Red
+    2-Blue
+    3-Green
+    4-yellow
+    11-lightred
+    22-lightgreen
+    33-lightblue
+    44-lightyellow
+
+    '''
+    if color == 3:# RG[B]
+        print(Fore.BLUE+text,end="")
+    elif color == 33:# RG[B] ++
+        print(Fore.LIGHTBLUE_EX+text,end="")
+    elif color == 1:#[R]GB
+        print(Fore.RED+text,end="")
+    elif color == 11:#[R]GB++
+        print(Fore.LIGHTRED_EX+text,end="")
+    elif color == 2:#R[G]B
+        print(Fore.GREEN+text,end="")
+    elif color == 22:#R[G]B ++
+        print(Fore.LIGHTGREEN_EX+text,end="")
+    elif color == 4:#yellow
+        print(Fore.YELLOW+text,end="")
+    elif color == 9:#grey
+        print(Fore.CYAN+text,end="")
+    else:
+        print(Fore.RED+text,end="")
+    if enterln == True:
+        print("",end="\n")
+    return 0
 
 if __name__ == "__main__" :
     if main() != 0:
 #        raise Exception("An error occured.")
-        pass
+        confirm()
